2859852d0fda9a5e0a95d8f807e9d2af36212c44
Dana Griff <DanaGriff1201@gmail.com>
2022-01-19 16:02:48 +0200

    fix window calls and change them to MainApp (fix to previous commit)

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 import DAL
-from MainFrame import Window
+from MainFrame import MainFrame
 from MainApp import MainApp
 
 if __name__ == "__main__":
     data = DAL.retrieve_db()
 
-    window = Window()
-    MainApp(window, data)
+    mainFrame = MainFrame()
+    MainApp(mainFrame, data)
 
-    window.mainloop()
+    mainFrame.mainloop()
